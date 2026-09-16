109b5cb7aaeeecd26735fc67d4f800187870d41b
Miko1234567 <Bekmurat1980kz7@gmail.com>
2026-09-16 15:07:46 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -84,7 +84,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        // Тікелей Builder арқылы жасау
         Report pdfReport = new Report.Builder()
                 .title("Annual Report")
                 .author("Reim")
